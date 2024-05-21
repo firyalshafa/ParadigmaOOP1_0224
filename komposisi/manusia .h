386618dd29f398bd281e3d3ad956dc77b959827d
firyalshafa <shafafiryal782@gmail.com>
2024-05-21 16:45:32 +0700

    membuat class manusia

---
+++ komposisi/manusia .h
@@ -0,0 +1,19 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varjantung;
+
+	manusia(string PName) : name(pName) {
+		cout << name << "hidup\n";
+	}
+
+	~manusia() {
+		cout << name << "mati\n";
+	}
+
+};
+#endif
