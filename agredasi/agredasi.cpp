8111425c73b60f8cf8f2ef4bf07419ccd8da4e8c
firyalshafa <shafafiryal782@gmail.com>
2024-05-21 16:55:12 +0700

    memanggil fungsi utama anak dan ibu

--- agredasi/agredasi.cpp
+++ agredasi/agredasi.cpp
@@ -2,10 +2,35 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+
+#include "anak.h"
+#include "ibu.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dwi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
