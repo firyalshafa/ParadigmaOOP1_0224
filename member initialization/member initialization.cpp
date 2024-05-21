14785a3a3b4a00824e7f65eddce13b6d6975a056
firyalshafa <shafafiryal782@gmail.com>
2024-05-21 16:40:27 +0700

    memanggil fungsi utama

--- member initialization/member initialization.cpp
+++ member initialization/member initialization.cpp
@@ -15,12 +15,19 @@ public:
     mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
 
     }
+    mahasiswa() {
+        cout << "id =" << id << endl;
+        cout << "nama =" << nama << endl;
+        cout << "nilai =" << nilai << endl;
+
+    }
 
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    mahasiswa mhs(12, "asroni,"  90 . 5)
+        return 0;
 }
 
 
